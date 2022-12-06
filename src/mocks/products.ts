6450b672165e52c80e7a45e9e0006166b6c3a81a
Abimael Lovera <abimaellovera@gmail.com>
2022-12-05 22:21:19 -0300

    criando menu

--- src/mocks/products.ts
+++ src/mocks/products.ts
@@ -1,9 +1,9 @@
 export const products = [
   {
-    _id: '6372e040f52e37ef85fe2c5e',
+    _id: '637775639bc8feb8eac9ff93',
     name: 'Pizza quatro queijos',
     description: 'Deliciosa pizza quatro queijos com borda simples',
-    imagePath: '1668472896991-quatro-queijos.png',
+    imagePath: '1668735152737-quatro-queijos.png',
     price: 40,
     ingredients: [
       {
@@ -32,7 +32,7 @@ export const products = [
     _id: '6372e276a381106c0f854cb3',
     name: 'Coca cola',
     description: 'Coca cola lata geladinha topzera',
-    imagePath: '1668473462705-coca-cola.png',
+    imagePath: '1668771923805-coca-cola.png',
     price: 7,
     ingredients: [],
   },
