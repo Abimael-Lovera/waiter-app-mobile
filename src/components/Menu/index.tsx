9652479007ebd97c6fb55326e9775cf9c4913888
Abimael Lovera <abimaellovera@gmail.com>
2022-12-05 22:39:25 -0300

    Configurando intl

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,9 +1,10 @@
 import { FlatList } from 'react-native';
 
 import { products } from '../../mocks/products';
+import { formatCurrency } from '../../utils/formatCurrency';
 import { Text } from '../Text';
 
-import { Product, Image, ProductDetails } from './styles';
+import { Product, ProductImage, ProductDetails } from './styles';
 
 export function Menu() {
   return(
@@ -15,16 +16,20 @@ export function Menu() {
         keyExtractor={product => product._id}
         renderItem={({ item: product }) =>(
           <Product>
-            <Image
+            <ProductImage
               source={{
                 uri: `http://192.168.15.16:3001/uploads/${product.imagePath}`,
               }}
             />
 
             <ProductDetails>
-              <Text>{product.name}</Text>
-              <Text>{product.description}</Text>
-              <Text>{product.price}</Text>
+              <Text weight="600">{product.name}</Text>
+              <Text size={14} color="#666666" style={{ marginVertical: 8 }}>
+                {product.description}
+              </Text>
+              <Text size={14} color="#333" weight="600">
+                {formatCurrency(product.price)}
+              </Text>
             </ProductDetails>
           </Product>
         )}
