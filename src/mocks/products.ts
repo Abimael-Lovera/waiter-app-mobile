7dfa0d582b8f9fbcab1901cbdc33352b1ab6b15d
Abimael Lovera <abimaellovera@gmail.com>
2022-12-07 23:11:45 -0300

    Alteramdo Header

--- src/mocks/products.ts
+++ src/mocks/products.ts
@@ -36,4 +36,41 @@ export const products = [
     price: 7,
     ingredients: [],
   },
+  {
+    _id: '6372e276a381as6c0f854cb3',
+    name: 'Coca cola',
+    description: 'Coca cola lata geladinha topzera',
+    imagePath: '1668771923805-coca-cola.png',
+    price: 7,
+    ingredients: [],
+  },
+  {
+    _id: '637775639bc8121feb8eac9ff93',
+    name: 'Pizza quatro queijos',
+    description: 'Deliciosa pizza quatro queijos com borda simples',
+    imagePath: '1668735152737-quatro-queijos.png',
+    price: 40,
+    ingredients: [
+      {
+        name: 'Mussarela',
+        icon: '🧀',
+        _id: '6372e040f52e37ef85fe2c5f'
+      },
+      {
+        name: 'Parmesão',
+        icon: '🧀',
+        _id: '6372e040f52e37ef85fe2c60'
+      },
+      {
+        name: 'Gouda',
+        icon: '🧀',
+        _id: '6372e040f52e37ef85fe2c61'
+      },
+      {
+        name: 'Brie',
+        icon: '🧀',
+        _id: '6372e040f52e37ef85fe2c62'
+      }
+    ],
+  },
 ];
